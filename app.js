2216db2a9a466a6d8914005c280ea87daf8f427b
pedramsheikhrahimi <pedr012G7sh@gmail.com>
2022-06-27 01:50:30 -0700

    feat: organize restaurant routes
    This organizes '/confirm', '/recommend', and restaurant routes.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 
 const defaultRoutes = require('./routes/default');
+const restaurantRoutes = require('./routes/restaurants');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -12,21 +13,6 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.use('/', defaultRoutes);
-
-app.get('/confirm', function (req, res) {
-	res.render('confirm');
-});
-
-app.get('/recommend', function (req, res) {
-	res.render('recommend');
-});
-
-app.get('/restaurants', function (req, res) {
-	res.render('restaurants');
-});
-
-app.get('/restaurants/r1', function (req, res) {
-	res.render('restaurant-detail');
-});
+app.use('/', restaurantRoutes);
 
 app.listen(3000);
